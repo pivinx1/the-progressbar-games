d9b11491dc638f25f485d8b6bb71347aca511ea0
markverb1 <markverb4@gmail.com>
2024-07-25 19:11:44 +0300

    moar

---
+++ data.gd
@@ -0,0 +1,94 @@
+extends Control
+
+# Enums
+
+enum TraitType {
+	POSITIVE,
+	NEUTRAL,
+	SPECIES,
+	NEGATIVE
+}
+
+enum Gender {
+	MALE,
+	FEMALE,
+	OTHER
+}
+
+enum Species {
+	PERSON,
+	SYNTH,
+	CATPERSON,
+	FURRY,
+	PROTOGEN, # It's the motherfucker from fortnite (fortnite)
+	UNKNOWN,
+	SHEEP,
+	ZOMBIE,
+	GHOST
+}
+
+enum GearType {
+	# Body
+	HEAD,
+	SKIN,
+	LEGS,
+	# Cosmetic
+	HAT,
+	SHIRT,
+	PANTS,
+	# Armor
+	HELMET,
+	VEST,
+	LEG_PROTECTION,
+}
+
+# Classes
+
+class Member:
+	var member_name = ""
+	var gender: Gender = Gender.OTHER
+	var species: Species = Species.PERSON
+
+class Team:
+	var hated: bool = false
+
+# Variables
+
+var information = {
+	"items" = {
+		
+	},
+	"gear" = {
+		
+	},
+}
+
+var teams = {
+	
+}
+var members = {
+	
+}
+
+# Funcs
+
+func remove_whitespace(str: String):
+	var cull = RegEx.new()
+	cull.compile("(\\W)+")
+	return cull.sub(str,"",true)
+
+func has_letters(str:String):
+	var hletters = RegEx.new()
+	hletters.compile("(\\w)+")
+	if hletters.search(str(str)):
+		return true
+	return false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	print(remove_whitespace("skibidi gyatt ohio rizzler!!/.aaa1244"))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	pass
---
+++ teams.gd
@@ -0,0 +1,32 @@
+extends Panel
+
+var text: String = ""
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_add_team_pressed():
+	pass # Replace with function body.
+
+
+func _on_remove_team_pressed():
+	pass # Replace with function body.
+
+
+func _on_rename_team_pressed():
+	pass # Replace with function body.
+
+
+func _on_hated_tick_toggled(toggled_on):
+	pass # Replace with function body.
+
+
+func _on_text_text_changed(new_text):
+	pass # Replace with function body.
