466b43d24be4f94a4236f1a18a634068cb49777c
pivinx1 <63850629+pivinx1@users.noreply.github.com>
2024-08-08 15:50:41 +0200

    prep for making action system
    had to make a skeleton UI, can't get anywhere without it, sorry Mark

---
+++ scripts/actionMain.gd
@@ -0,0 +1,10 @@
+extends Panel
+
+@export var logField: RichTextLabel		# using camelCase so I don't get RSI or something
+
+func _ready():
+	pass # Replace with function body.
+
+
+func _process(delta):
+	pass
