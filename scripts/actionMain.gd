7d2a469789f582283f386747868504c97d8d9e40
pivinx1 <63850629+pivinx1@users.noreply.github.com>
2024-08-08 16:17:31 +0200

    dice rolling and other things
    stuff in modifierDiceRoll is commented out to avoid errors since we don't fetch skills yet because there are none to fetch

--- scripts/actionMain.gd
+++ scripts/actionMain.gd
@@ -1,10 +1,26 @@
 extends Panel
 
-@export var logField: RichTextLabel		# using camelCase so I don't get RSI or something
+# Instantiation of stuff
+var ex = Expression.new()
+
+# Constants
+const diceFloor: int = 1
+
+# Exports
+@export var logField: RichTextLabel	 # using camelCase so I don't get RSI or something
 
 func _ready():
-	pass # Replace with function body.
+	randomize()		# prepare the dice
 
 
-func _process(delta):
-	pass
+func rollDice(d: int):	# generic roll
+	return randf_range(diceFloor, d)	# "dice" roll for generic actions
+
+func modifierDiceRoll(d: int, skill: String):	# used for ability checks
+	# var modifier: int = calculateModifier(skills[skill])
+	var rollResult: int = rollDice(d)
+	# return rollResult + modifier
+
+func calculateModifier(skillLevel: int):
+	ex.parse("(x - 10) / 2", ["x"])
+	return ex.execute([skillLevel])
